c56ef8912cd9e9fdc2c22fbc97abe2afee9e6301
xiaohui <2535418266@qq.com>
2018-11-29 14:41:20 +0800

    changed dir struct

--- ports/k210-freertos/pack/pack_zip.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd bin_file
-zip -r ./firmware.zip ./* 
-mv firmware.zip ../sipeedm1.kfpkg
--- ports/k210-freertos/spiffs-port/include/spiffs-port.h
+++
@@ -1,9 +0,0 @@
-#ifndef MYSPIFFS_H
-#define MYSPIFFS_H
-
-#include <spiffs.h>
-void my_spiffs_mount();
-void my_spiffs_init();
-int format_fs(void);
-extern spiffs fs;
-#endif// MYSPIFFS_H
