b716eaa49c8f09ae94a604978878042b5dcd47c8
xiaohui <2535418266@qq.com>
2018-12-13 15:47:17 +0800

    edit k210-freertos/docs/README.d

--- ports/k210-freertos/docs/README.d
+++ ports/k210-freertos/docs/README.d
@@ -4,3 +4,4 @@
 ## Verify that each module's modifications are compiled successfully and connected to the output file
 ## Please manually add the used header file path at the beginning of each layer of Makefile.
 ## The Makefile in the mpy_support directory will search all source code files (including subdirectories) in the current directory.
+## Verifying that the cpp source code is compiled properly, and that the cpp feature is supported normally.
